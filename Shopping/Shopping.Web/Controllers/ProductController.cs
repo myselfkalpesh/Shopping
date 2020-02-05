24b0189ca045282378eaa4a9c49b15f2e0af416b
Kalpesh <myselfkalpesh@yahoo.com>
2020-02-05 13:19:06 -0500

    Product Controller Built and Tested

---
+++ Shopping/Shopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,124 @@
+﻿using Shopping.Core.Models;
+using Shopping.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository context;
+
+        public ProductController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(p);
+            }
+            else
+            {
+                context.Insert(p);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(p);
+                }
+
+                productToEdit.Category = p.Category;
+                productToEdit.Description = p.Description;
+                productToEdit.Image = p.Image;
+                productToEdit.Name = p.Name;
+                productToEdit.Price = p.Price;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
