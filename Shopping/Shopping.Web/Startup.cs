b7baca05534c56d3258c1f1eb4fad5c121beec99
Kalpesh <myselfkalpesh@yahoo.com>
2020-02-05 12:28:34 -0500

    Initial Commit

---
+++ Shopping/Shopping.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shopping.Web.Startup))]
+namespace Shopping.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
