cb3dc16b8ddc912c1ea8b640b8d465ba7ff1614e
Kalpesh <myselfkalpesh@yahoo.com>
2020-02-05 21:37:05 -0500

    Added Product Listing

--- Shopping/Shopping.Web/Controllers/HomeController.cs
+++ Shopping/Shopping.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shopping.Core;
+using Shopping.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,35 @@ namespace Shopping.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productContext;
+        IRepository<ProductCategory> categoryContext;
+
+        public HomeController(IRepository<Product> _productContext, IRepository<ProductCategory> _categoryContext)
+        {
+            productContext = _productContext;
+            categoryContext = _categoryContext;
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = productContext.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = productContext.Find(Id);
+
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+            
         }
 
         public ActionResult About()
